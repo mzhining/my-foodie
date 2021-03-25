fbea1cdb9cdb81ea4956ecb8bc539ae6ad9f820a
mzhining <71877471+mzhining@users.noreply.github.com>
2021-03-25 23:47:38 +0800

    Updated main.js with successful authentication
    routes.js no longer imported, add the paths directly in main.js instead.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,46 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import Routes from './routes.js'
-// import firebase from 'firebase';
+// import Routes from './routes.js'
+import firebase from 'firebase';
+
+// from file: routes.js
+import Home from './components/Home.vue'
+// import Delivery from './components/Delivery.vue'
+import Reservation from './components/Reservation.vue'
+// import Pickup from './components/Pickup.vue'
+import CustSignup from './components/CustSignup.vue'
+import RestSignup from './components/RestSignup.vue'
+import SignupSuccess from './components/SignupSuccess.vue'
+import Login from './components/Login.vue'
+import Register from './components/Register.vue'
+// end of import from routes.js
+
+import DashboardC from './components/DashboardC.vue';
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 const myRouter = new VueRouter({
 // let myRouter = new VueRouter({
-  routes: Routes,
+  routes: [
+    { path: '/', component: Home, redirect: '/login' },
+    // { path: '/delivery', component: Delivery },
+    // { path: '/reservation', component: Reservation},
+    { path: '/reservation', component: Reservation, meta: {requiresAuth: true}},
+    // { path: '/pickup', component: Pickup }
+    // { path: '/signup-cust', component: CustSignup},
+    { path: '/signup-cust', component: CustSignup, meta: {requiresGuest: true}},
+    // { path: '/signup-rest', component: RestSignup},
+    { path: '/signup-rest', component: RestSignup, meta: {requiresGuest: true}},
+    { path: '/signup-success', component: SignupSuccess},
+    // { path: '/login', name: 'login', component: Login},
+    { path: '/login', name: 'login', component: Login, meta: {requiresGuest: true}},
+    { path: '/register', name: 'register', component: Register, meta: {requiresGuest: true}},
+
+    { path: '/account', component: DashboardC, meta: {requiresAuth: true}}
+
+  ],
   mode: 'history'
 });
 
@@ -51,3 +82,14 @@ new Vue({
 // });
 
 // export default myRouter;
+
+myRouter.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !currentUser) next('login');
+  else if (!requiresAuth && currentUser) next('account');
+  else next();
+});
+
+export default myRouter;
