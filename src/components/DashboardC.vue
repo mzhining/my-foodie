d551897431c7949e992c739b5b84130aff3a37e5
LiuYanJulia <e0376941@u.nus.edu>
2021-03-28 22:02:21 +0800

    update dashboard/order/delivery

--- src/components/DashboardC.vue
+++ src/components/DashboardC.vue
@@ -1,18 +1,29 @@
 <template> 
     <div >
+        <div class="kotak-mereng">
+            <div class="penyot">
+                <div class="rectangle" />
+                <div class="segitiga" />
+            </div>
+            
+            <div class="kotak-mereng-content">
+                <img id="pic" src="@/assets/breakfast.jpg" alt="Breakfast">
+                <p id="slogan">One Stop Place for Your Stomach! </p>
+            </div>
+        </div>
+        <hr id="line">
         <h1>My Profile</h1>
         <h2>Favourite Restaurant</h2>
         <div id="fav">
-            <li v-for="item in fav" v-bind:key="item.id">
+            <li v-for="item in fav" v-bind:key="item.id" class="box">
                 <span>{{item}}</span>
-                <br><br>
             </li>
+            <br><br>
             <!-- how to disply picture using a fr loop, now the array fav_img has all element as the weblink-->
-            <!--<li v-for="item in fav_img" v-bind:key="item.id"> 
-                <img src="item" alt="error" width="200" height="200"></img>
-                <br>
+            <li v-for="item in fav_img" v-bind:key="item.id" class="box"> 
+                <img v-bind:src="item" alt="error"/> 
+                <span>     </span>
             </li>
-            -->
         </div>
         <h2>Current Order</h2>
         <div class="section">
@@ -36,6 +47,7 @@
         <div id="blank">
             <p> </p>
         </div>
+        <router-link to="/accountR" exact>Go to DashboardR </router-link>
     </div>
 </template>
 
@@ -111,6 +123,70 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@700&display=swap');
+
+.home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.kotak-mereng {
+    position: relative;
+    height: 20rem;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+}
+
+.kotak-mereng-content {
+    max-width: 1300px;
+    position: relative;
+    width: 100%;
+}
+.rectangle{
+    height: 5rem;
+    background-color: rgba(224, 116, 114, 0.64);
+}
+
+.segitiga {
+    overflow: hidden;
+    height: 0;
+    border-width: 10rem 100vw 0 0;
+    border-style: solid;
+    border-color: rgba(224, 116, 114, 0.64) transparent transparent rgba(224, 116, 114, 0.64);
+}
+
+.penyot {
+    position: absolute;
+    overflow: hidden;
+    top: 0;
+    width: 100%;
+    z-index: 0;
+}
+
+#slogan {
+    font-family: Carosello;
+    font-size: 40px;
+    color: white;
+    margin: 0;
+    position: absolute;
+    z-index: 69;
+    left: 10%;
+    top: 30%;
+}
+
+#pic {
+    height: 17rem;
+    position: absolute;
+    z-index: 69;
+    right: 8%;
+    top: 8%;
+    height: 15rem;
+}
+#line {
+    border: 3px dashed #90141C;
+}
+
 h1 {
   font-size:30px;
   color:black;
@@ -163,7 +239,11 @@ h2 {
 .blank {
     height:700px;
 }
-
+.box { 
+    display: inline; 
+    margin-left: 5px;
+    margin-right: 5px;
+}
 
 </style>
 
