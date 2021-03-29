06d235d4d7a783326710f1a29c1ff44fc36d0742
lottiezh <zhenghao98@gmail.com>
2021-03-30 01:01:58 +0800

    added basket and reservation order page

---
+++ src/components/Basket.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <p>Menu:</p>
+        <ul id="list">
+            <li id="list" v-for="item in itemsSelected" v-bind:key="item.id">
+                <p>{{item[0]}} x {{item[1]}} </p>
+            </li>
+        </ul>
+        <br>
+        <button @click="findTotal"> Calculate Total </button>
+        <br>
+        <p v-show="show"> Subtotal: ${{subtotal}} <br> Grand Total: ${{grandtotal}} </p>
+        <br><br>
+        <button v-on:click="sendOrder"> Add Order </button>
+    </div>
+</template>
+
+<script>
+    import database from '../firebase.js'
+
+    export default {
+        props: {
+            itemsSelected: {
+                type: Array
+            }
+        },
+        data() {
+            return {
+                subtotal: 0,
+                show: false,
+            }
+        },
+        computed: {
+            grandtotal: function() {
+                var rounding = this.subtotal * 1.07;
+                return rounding.toFixed(2);
+            }
+        },
+        methods: {
+            findTotal() {
+                this.show = true;
+                var sub = 0;
+                for (var i = 0; i < this.itemsSelected.length; i++) {
+                    sub += this.itemsSelected[i][2] * this.itemsSelected[i][1];
+                }
+                this.subtotal = sub;
+            },
+            sendOrder() {
+                var newOrder = {
+                    //item: quantity
+                };
+                this.itemsSelected.forEach((item) => {
+                    newOrder[item[0]] = item[1];
+                });
+                //add order to database
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
