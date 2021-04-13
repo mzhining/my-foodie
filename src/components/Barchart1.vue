025b2303ac0ac926d6e2b92d53085a30bb0ecc21
LiuYanJulia <e0376941@u.nus.edu>
2021-04-14 00:10:12 +0800

    create chart

---
+++ src/components/Barchart1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="chart">
+    <h1>Bar Chart</h1>
+    <chart></chart>
+  </div>
+</template>
+
+<script>
+import Chart from "./BarChart.js";
+export default {
+  components: {
+    Chart
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: #a8c9d5;
+  font-size:100px;
+  color: white;
+  text-align: center;
+  height:100px;
+}
+</style>
