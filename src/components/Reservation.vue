b23d66c5bf31a55a185f94885e9e6cdb3b3149cc
LiuYanJulia <e0376941@u.nus.edu>
2021-04-19 16:04:24 +0800

    remove reload operation

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -98,7 +98,7 @@ export default {
             }
             database.collection('customers').doc(this.$userUid).update({
                 favourites: fav
-            }).then(() => {location.reload()});
+            });
                 
         },
         fetchItems: function() {
